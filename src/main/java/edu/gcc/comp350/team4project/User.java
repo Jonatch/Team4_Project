eb73750ffb52222873aa6df8c7d1a00b21261b49
Jonatch <115596067+Jonatch@users.noreply.github.com>
2023-02-22 14:44:00 -0500

    Setting up git

---
+++ src/main/java/edu/gcc/comp350/team4project/User.java
@@ -0,0 +1,7 @@
+package edu.gcc.comp350.team4project;
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("This is my user class.");
+    }
+}
---
+++ src/test/java/edu/gcc/comp350/team4project/UserTest.java
@@ -0,0 +1,7 @@
+package edu.gcc.comp350.team4project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+}
