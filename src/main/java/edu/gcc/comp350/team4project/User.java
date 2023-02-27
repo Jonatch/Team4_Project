f4466220d43e155d431ee63d01a2ca30e48b237a
WEIRJR20 <jonathanweir34@gmail.com>
2023-02-27 14:14:57 -0500

    test

--- src/main/java/edu/gcc/comp350/team4project/User.java
+++ src/main/java/edu/gcc/comp350/team4project/User.java
@@ -1,7 +1,9 @@
 package edu.gcc.comp350.team4project;
 
 public class User {
-    public static void main(String[] args) {
-        System.out.println("This is my user class.");
+
+
+    public String toString(){
+        return "string";
     }
 }
