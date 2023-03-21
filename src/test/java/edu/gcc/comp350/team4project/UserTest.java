c688f385b3f2bfb39612ec8e3bebd94dfe06f2a5
Jonatch <115596067+Jonatch@users.noreply.github.com>
2023-03-21 14:37:29 -0400

    Changes in User.java: *Users now have the functionality of adding and removing schedules from their profiles. *Improvements can be made to adding and removing as we get further into the compilation and running of the app
    Changes for tests:
    *Program passes tests for all constructors and methods within Users.java, including adding schedules, logging in, etc.

--- src/test/java/edu/gcc/comp350/team4project/UserTest.java
+++ src/test/java/edu/gcc/comp350/team4project/UserTest.java
@@ -1,7 +1,78 @@
 package edu.gcc.comp350.team4project;
 
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserTest {
+public class UserTest {
+
+    @Test
+    public void testConstructor() throws Exception {
+        User user = new User("testuser", "2023", "testpassword", false);
+        assertEquals("testuser", user.getUsername());
+        assertEquals("2023", user.getYear());
+        assertEquals("testpassword", user.getPassword());
+        assertFalse(user.isGuest());
+    }
+
+    @Test
+    public void testDuplicateUsername() throws Exception {
+        User user1 = new User("testuser", "2023", "testpassword", false);
+        assertThrows(Exception.class, () -> new User("testuser", "2024", "testpassword2", false));
+    }
+
+    @Test
+    public void testAuthenticate() throws Exception {
+        assertFalse(User.authenticate("nonexistentuser", "testpassword"));
+        User user = new User("testuser", "2023", "testpassword", false);
+        assertTrue(User.authenticate("testuser", "testpassword"));
+        assertFalse(User.authenticate("testuser", "wrongpassword"));
+    }
+
+    // Test case for addSchedule method with FALL semester
+    @Test
+    public void testAddSchedule_Fall() throws Exception {
+        User user = new User("username", "year", "password", false);
+        user.addSchedule("Schedule 1", Semester.FALL);
+        assertEquals(1, user.getSchedules().size());
+        assertEquals("Schedule 1", user.getSchedules().get(0).getScheduleName());
+        assertEquals(Semester.FALL, user.getSchedules().get(0).getSemester());
+    }
+
+    // Test case for addSchedule method with SPRING semester
+    @Test
+    public void testAddSchedule_Spring() throws Exception {
+        User user = new User("username", "year", "password", false);
+        user.addSchedule("Schedule 2", Semester.SPRING);
+        assertEquals(1, user.getSchedules().size());
+        assertEquals("Schedule 2", user.getSchedules().get(0).getScheduleName());
+        assertEquals(Semester.SPRING, user.getSchedules().get(0).getSemester());
+    }
+
+    // Test case for removeSchedule method with FALL semester
+    @Test
+    public void testRemoveSchedule_Fall() throws Exception {
+        User user = new User("username", "year", "password", false);
+        user.addSchedule("Schedule 1", Semester.FALL);
+        user.addSchedule("Schedule 2", Semester.FALL);
+        user.removeSchedule(1);
+        assertEquals(1, user.getSchedules().size());
+        assertEquals("Schedule 1", user.getSchedules().get(0).getScheduleName());
+        assertEquals(Semester.FALL, user.getSchedules().get(0).getSemester());
+    }
+
+    // Test case for removeSchedule method with SPRING semester
+    @Test
+    public void testRemoveSchedule_Spring() throws Exception {
+        User user = new User("username", "year", "password", false);
+        user.addSchedule("Schedule 1", Semester.SPRING);
+        user.addSchedule("Schedule 2", Semester.SPRING);
+        user.removeSchedule(0);
+        assertEquals(1, user.getSchedules().size());
+        assertEquals("Schedule 2", user.getSchedules().get(0).getScheduleName());
+        assertEquals(Semester.SPRING, user.getSchedules().get(0).getSemester());
+    }
 
 }
