f312af074f9faa260a3a75eca8d9f953ee8713d4
Jonatch <115596067+Jonatch@users.noreply.github.com>
2023-03-28 21:34:03 -0400

    We can now pull from the database allowing us to update, authenticate, etc.

--- src/main/java/edu/gcc/comp350/team4project/StoreContents.java
+++ src/main/java/edu/gcc/comp350/team4project/StoreContents.java
@@ -6,8 +6,91 @@ import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class StoreContents {
+
+    public static boolean authenticateUser(String name, String password) {
+        String sql = "SELECT username, password from Users WHERE username = ?";
+        try (Connection conn = StoreContents.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, name);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            String username = "";
+            String pass = "";
+            while (rs.next()) {
+                username = rs.getString("username");
+                pass = rs.getString("password");
+            }
+
+            if (username.equals(name) && pass.equals(password)) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+
+
+    public static void pullUser(String name){
+        String sql = "SELECT * FROM Users WHERE username = ?";
+        String username = "", year = "", password = "", schedules = "";
+        String schedName = "";
+
+        try (Connection conn = StoreContents.connect();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, name);
+
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                username = rs.getString("username");
+                 year = rs.getString("year");
+                 password = rs.getString("password");
+                schedules = rs.getString("schedules");
+            }
+            User pulledUser = new User(username, year, password, false);
+            Scanner sc = new Scanner(schedules);
+            //TODO: Loop through sc to find + create the schedule and course info
+
+
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void updateUser(User user){
+        String sql = "UPDATE Users SET schedules = ? WHERE username = ?";
+
+        String stringSchedules = "";
+        for (Schedule sched : user.getSchedules()){
+            String temp = sched + "\n\n";
+            stringSchedules += temp;
+        }
+
+        try (Connection conn = StoreContents.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, stringSchedules);
+            pstmt.setString(2, user.getUsername());
+
+            int rowsUpdated = pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
     /**
      * @param fileName the database file name
      */
@@ -27,7 +110,7 @@ public class StoreContents {
         }
     }
 
-    private Connection connect() {
+    private static Connection connect() {
         // SQLite connection string
         String url = "jdbc:sqlite:C://sqlite/db/team4_project.db";
         Connection conn = null;
@@ -64,7 +147,7 @@ public class StoreContents {
 
 
 
-    public void insert(String username, String year, String password, ArrayList<Schedule> schedules) {
+    public static void insert(String username, String year, String password, ArrayList<Schedule> schedules) {
         String sql = "INSERT INTO Users(username,year,password,schedules) VALUES(?,?,?,?)";
         String stringSchedules = "";
         for (Schedule sched : schedules){
@@ -72,7 +155,7 @@ public class StoreContents {
             stringSchedules += temp;
         }
 
-        try (Connection conn = this.connect();
+        try (Connection conn = StoreContents.connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, username);
             pstmt.setString(2, year);
@@ -102,7 +185,7 @@ public class StoreContents {
     /**
      * @param args the command line arguments
      */
-//    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 //        ArrayList<DayOfWeek> testDays = new ArrayList<DayOfWeek>();
 //        testDays.add(DayOfWeek.MONDAY);
 //        testDays.add(DayOfWeek.WEDNESDAY);
@@ -111,25 +194,32 @@ public class StoreContents {
 //                "test course", 3, "Dr. test", "a test course", testDays, LocalTime.of(10,0,0), LocalTime.of(10,50,0));
 //        Course test1 = new Course(1, "COMP", Semester.FALL, "300", 'A',
 //                "test course", 3, "Dr. test", "a test course", testDays, LocalTime.of(9,0,0), LocalTime.of(9,50,0));
-//        Course test3 = new Course(2, "COMP", Semester.SPRING, "400", 'A',
+//        Course test3 = new Course(20, "EDUC", Semester.SPRING, "400", 'A',
 //                "test course", 3, "Dr. test", "a test course", testDays, LocalTime.of(10,0,0), LocalTime.of(10,50,0));
-//        Course test4 = new Course(1, "COMP", Semester.SPRING, "300", 'A',
+//        Course test4 = new Course(10, "EDUC", Semester.SPRING, "300", 'A',
 //                "test course", 3, "Dr. test", "a test course", testDays, LocalTime.of(9,0,0), LocalTime.of(9,50,0));
 //        createNewDatabase("team4_project.db");
 //        drop();
 //        createNewTable();
 //        StoreContents users = new StoreContents();
 //        User jonah = new User("DybasJW20", "Junior", "testpass", false);
-//        Schedule test = new Schedule("test_sched", Semester.FALL);
+//        Schedule test = new Schedule("newer_test_sched", Semester.SPRING);
 //        Schedule test_2 = new Schedule("test_sched2", Semester.SPRING);
-//        test.addCourse(test1);
-//        test.addCourse(test2);
+//        test.addCourse(test3);
+//        test.addCourse(test4);
 //        test_2.addCourse(test3);
 //        test_2.addCourse(test4);
+
+//        System.out.println(jonah.getSchedules());
 //        jonah.saveScheduleToUser(test);
-//        jonah.saveScheduleToUser(test_2);
-//
-//        users.insert("DybasJW20", "Junior", "testpass", jonah.getSchedules());
-//
-//    }
+//        users.updateUser(jonah);
+//        users.insert(jonah.getUsername(), "Junior", "testpass", jonah.getSchedules());
+//        pullUser(jonah.getUsername());
+//        updateUser(jonah);
+//        if (users.authenticateUser("DybasJW20", "testpass") == true) {
+//            System.out.println("Autheticated!");
+//        }else{
+//            System.out.println("Failed to authenticate");
+//        }
+    }
 }
